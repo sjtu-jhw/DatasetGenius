1f592e64c7bfb92af2772a7d7ef8798e04902d09
sjtu-jhw <jhw15763365981@gmail.com>
2024-03-24 15:44:31 +0800

    replace \input{.*} into main .tex file

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -6,6 +6,7 @@ import os
 import subprocess
 from multiprocessing import Pool
 import regex
+import numpy
 
 # Fix for Windows: Even if '\' (os.sep) is the standard way of making paths on
 # Windows, it interferes with regular expressions. We just change os.sep to '/'
@@ -39,6 +40,24 @@ def _get_absolute_path(path: str) -> str:
     
     return absolute_path
 
+def _list_all_files(in_folder):
+  """
+  Return all files' full path in a list
+  """
+  to_consider = [
+      os.path.join(path, name)
+      for path, _, files in os.walk(in_folder)
+      for name in files
+  ]
+  return to_consider
+
+def _read_file_content(file_path):
+  """
+  Return file contents in string
+  """
+  with open(file_path, 'r', encoding='ISO-8859-1') as fp: # not using utf-8 to avoid UnicodeDecodeError
+    contents = fp.read()
+    return contents
 
 def arxiv_latex_cleaner_pre(path: str) -> None:
     """
@@ -54,18 +73,7 @@ def arxiv_latex_cleaner_pre(path: str) -> None:
     print(f"PID {os.getpid()} done.")
     return None
 
-def _list_all_files(in_folder):
-  to_consider = [
-      os.path.join(path, name)
-      for path, _, files in os.walk(in_folder)
-      for name in files
-  ]
-  return to_consider
 
-def _read_file_content(filename):
-  with open(filename, 'r', encoding='ISO-8859-1') as fp: # not using utf-8 to avoid UnicodeDecodeError
-    contents = fp.read()
-    return contents
 
 def check_main_tex(in_folder: str):
    """
@@ -87,6 +95,67 @@ def check_main_tex(in_folder: str):
          main_tex_name.append(file_name)
    assert len(main_tex_name) == 1, "There are more than one main .tex file in the folder."
    return main_tex_name[0]
+
+
+
+def find_input_tex(in_tex: str):
+   """
+   Find \\input{.*} in input tex strings so that we can replace them with original tex text.
+
+   Args:
+        in_tex(str): all tex text in the .tex file
+
+   Returns:
+        list of .tex files
+   """
+   pattern = r"\\input{(.*)}"
+   p = regex.compile(pattern)
+   finds = p.findall(in_tex)
+   return finds
+   
+def insert_input_tex(in_tex_path: str) -> None:
+   """
+   Find \\input{.*} in input tex files and substitute them with contents within corresponding .tex files
+   +
+   Rewrite in_tex_path contents
+
+   Args:
+        in_tex_path(str): path to .tex file where \\input{.*} may exist
+
+   Returns:
+        None
+   """
+   with open(in_tex_path, "r", encoding='ISO-8859-1') as f:
+      in_tex = f.read()
+   finds = find_input_tex(in_tex) # matched patterns
+   for find in finds:
+      insert_tex_path = os.path.join(os.path.dirname(in_tex_path), find+'.tex') # HACK: believe input files are .tex files
+      insert_tex_content = _read_file_content(insert_tex_path)
+      in_tex = in_tex.replace(f"\\input{{{find}}}", insert_tex_content)
+   with open(in_tex_path, "w", encoding='ISO-8859-1') as f:
+      f.write(in_tex)
+
+def insert_input_tex_completely(in_tex_path: str) -> None:
+   """
+   In case of those needed to be inserted .tex file need to be inserted.
+
+   Args:
+        in_tex_path(str): path to .tex file where \\input{.*} may exist
+
+   Returns:
+        None
+   """
+   while True:
+    with open(in_tex_path, "r", encoding='ISO-8859-1') as f:
+        in_tex = f.read()
+    finds = find_input_tex(in_tex) # matched patterns
+    if len(finds) == 0:
+        break
+    else:
+        insert_input_tex(in_tex_path)
+      
+      
+   
 
 
 # for pre-cleaning the latex code directory, removing unused files and commets in the .tex files.
@@ -116,6 +185,7 @@ if __name__ == "__main__":
        for paper_dir in os.listdir(path_):
           if paper_dir.endswith("_arXiv"):
             paper_dirs.append(os.path.join(path_, paper_dir))
-    for path in paper_dirs:
-        print(check_main_tex(path))
+    # for path in paper_dirs:
+    #     print(check_main_tex(path))
+    insert_input_tex_completely(check_main_tex(paper_dirs[3]))
 
